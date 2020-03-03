5d62e6e1f74cb61f246f29ca712f600e3c45a4ba
Thiago Franceschinelli <tfranceschinelli@daitan.com>
2020-03-03 17:00:50 -0300

    setup

---
+++ build/precache-manifest.1129e69c0997f8302a463447afb2389c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05bc4bfe976115c5c2d95c559ba45011",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a241d569a86d926760b",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c511fc8cc3d144bb2ba4",
+    "url": "/static/js/2.02dde09f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.02dde09f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a241d569a86d926760b",
+    "url": "/static/js/main.ceda8e0e.chunk.js"
+  },
+  {
+    "revision": "9fd0d41aa12e2027ae07",
+    "url": "/static/js/runtime-main.8fa56997.js"
+  }
+]);
