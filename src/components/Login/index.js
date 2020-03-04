6a4af6d99ad711e5457ea0e63f1475ab062d5f92
Thiago Franceschinelli <tfranceschinelli@daitan.com>
2020-03-04 10:42:43 -0300

    change to functional and hooks

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,23 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from "../Button";
 
-class Login extends React.Component {
-
-  constructor(props) {
-    super(props);
-  }
-
-    render() {
-      return (
-        <React.Fragment>
-          <h3>Sign In</h3>
-          <input label="name" type="text" />
-          <input label="password" type="text" />
-          <Button>bla</Button>
-          
-        </React.Fragment>
-      );
-    }
-  }
+function Login() {
+  const [state, setState] = useState(0);
+  return (
+    <React.Fragment>
+      <h3>Sign In</h3>
+      <input label="name" type="text" />
+      <input label="password" type="text" />
+      <Button>bla</Button>
+      
+    </React.Fragment>
+  );
+}
 
 export default Login;
--- src/components/ProductsList/index.js
+++ src/components/ProductsList/index.js
@@ -1,23 +1,16 @@
 import React from 'react';
 
-class ProductsList extends React.Component {
-
-  constructor(props) {
-    super(props);
-  }
-
-    render() {
-      return (
-        <div className="shopping-list">
-          <h1>ProductsList for {this.props.name}</h1>
-          <ul>
-            <li>p1</li>
-            <li>p2</li>
-            <li>p3</li>
-          </ul>
-        </div>
-      );
-    }
-  }
+function ProductsList() {
+  return (
+    <div className="shopping-list">
+      <h1>ProductsList for {this.props.name}</h1>
+      <ul>
+        <li>p1</li>
+        <li>p2</li>
+        <li>p3</li>
+      </ul>
+    </div>
+  );
+}
 
 export default ProductsList;
