56b6adbafe5c30a31d7f01389719fb3542533bc7
Thiago Franceschinelli <tfranceschinelli@daitan.com>
2020-03-05 18:42:42 -0300

    extract to component

---
+++ src/components/ProductElement/index.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+import Button from "../Button";
+
+import { useTranslation } from 'react-i18next';
+import { CustomPlaceholder } from 'react-placeholder-image';
+
+
+function ProductElement({product}) {
+  // TODO criar custom hook
+  const [cartProducts, setCartProducts] = useState([]);
+  const { t } = useTranslation();
+
+  const addProduct = (product) => {
+    console.log('product added ' , product)
+    setCartProducts([
+      ...cartProducts,
+      product
+    ]);
+  }
+
+  return (
+    <li key={product._id}>
+      <CustomPlaceholder width={100} height={100} />
+      <span>{product.name}</span>
+      <Button label={t('productElement.add')} handleClick={()=>{addProduct(product)}}/>
+    </li>
+  );
+}
+
+export default ProductElement;
--- src/components/ProductsList/index.js
+++ src/components/ProductsList/index.js
@@ -1,16 +1,12 @@
 import React, { useState, useEffect } from 'react';
 
-import Button from "../Button";
+import ProductElement from "../ProductElement";
 import productService from '../../services/ProductService';
 
 import { useTranslation } from 'react-i18next';
-import { CustomPlaceholder } from 'react-placeholder-image';
 
-
-function ProductsList(props) {
+function ProductsList() {
   const [products, setProducts] = useState([]);
-  // TODO criar custom hook
-  const [cartProducts, setCartProducts] = useState([]);
   const { t } = useTranslation();
 
   useEffect(() => {
@@ -22,24 +18,12 @@ function ProductsList(props) {
     });
   });
 
-  const addProduct = (product) => {
-    console.log('product added ' , product)
-    setCartProducts([
-      ...cartProducts,
-      product
-    ]);
-  }
-
   return (
     <div className="shopping-list">
       <h1>{t('productsList.title')}</h1>
       <ul className="products-container no-list-style">
         {products.map(product => {
-          return <li key={product._id}>
-              <CustomPlaceholder width={100} height={100} />
-              <span>{product.name}</span>
-              <Button label={t('productsList.add')} handleClick={()=>{addProduct(product)}}/>
-            </li>;
+          return <ProductElement product={product} />;
         })}
       </ul>
     </div>
